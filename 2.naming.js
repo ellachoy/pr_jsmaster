728ae8d0fc3b089eeddd9879e1b00b9291530d08
ella choy <egal21de@gmail.com>
2022-03-25 01:06:44 +0100

    num

--- 2.naming.js
+++ 2.naming.js
@@ -14,3 +14,26 @@
  * 이모지 ❌
  * 여러개의 변수를 1, 2, 3 숫자로 구분 ❌ -> 최대한 의미있게, 구체적인 이름으로 작성!
  */
+ let apple;
+ let redApple;
+ 
+ // 나쁜예제 💩
+ let number = 20;
+ 
+ // 좋은예제 ✨
+ let myAge = 20;
+ 
+ // 나쁜예제 💩
+ let audio1;
+ let audio2;
+ 
+ // 좋은예제 ✨
+ let backgroundAudio;
+ let windAudio;
+ 
+ // 꿀팁! 🍯
+ let audioBackground;
+ let audioWind;
+ 
+ audioBackground;
+ 
