088786a9f9d79ec8950307b484576fda0ce6290b
ella choy <egal21de@gmail.com>
2022-03-28 01:44:27 +0200

    operators

---
+++ 2.operator/5_2.arithmetic.js
@@ -0,0 +1,23 @@
+// 산술 연산자 (Arithmetic operators)
+// + 더하기
+// - 빼기
+// * 곱하기
+// / 나누기
+// % 나머지값
+// ** 지수 (거듭제곱)
+console.log(5+2);
+console.log(5 - 2);
+console.log(5 * 2);
+console.log(5 / 2);
+console.log(5 % 2);
+console.log(5 ** 2); // es7
+console.log(5 ** 3); 
+console.log(Math.pow(5, 2));
+
+
+
+// + 연산자 주의점!
+let text = 'two' + 'words';
+console.log(text);
+text= '1'+ 1;
+console.log(text);
