9574da282c2a1165c3a5fa6ca92cfcbd85f697a8
ella choy <egal21de@gmail.com>
2022-03-29 22:13:36 +0200

    statements

---
+++ 1.if.js
@@ -0,0 +1,22 @@
+// 조건문 Conditional Statement
+// if(조건) { }
+// if(조건) { } else {}
+// if(조건1) { } else if(조건2) {} else {}
+
+
+// let fruit ='apple';
+let fruit ='orange';
+if (fruit === 'apple'){
+    console.log('🍎');
+ } 
+ else if (fruit === 'orange'){
+     console.log('🍊');
+ }
+ else{
+     console.log('🥨');
+ }
+ 
+ if (2 < 1) {
+    console.log('출력되면 안됨!');
+  }
+  
